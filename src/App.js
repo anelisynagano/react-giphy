43201ec5f42f1fe21c151e94634ce3fefd0b1096
WildCodeSchool <anelisynagano@gmail.com>
2020-07-02 20:51:11 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GifList from './components/GifList';
+import SelectedGif from './components/SelectedGif';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataArray: [],
+      selectedGif: "https://media1.giphy.com/media/rwNpHtaMGnStW/giphy.gif"
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  handleSelectGif = (clickedGif) => {
+    this.setState({ selectedGif: clickedGif })
+  }
+
+  fetchData = () => {
+    fetch('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=LI4QeV4xLGKRQ6n79ZN29o0MWcAPoFhn&limit=5')
+      .then(res => res.json())
+      .then(data => this.setState({ dataArray: data.data }))
+  }
+
+  render() {
+    const { selectedGif, dataArray } = this.state;
+    return (
+     <div className='container'>
+       <SelectedGif selectedGif={selectedGif} />
+       <GifList gifs={dataArray} handleSelectGif={this.handleSelectGif} />
+     </div>
+    );
+  }
 }
 
 export default App;
